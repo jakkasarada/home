daf1f524bb8378fb95db6fbc458c12a38da88126
jakka sharada <sharadas672@gmail.com>
2022-08-15 15:35:59 +0530

    2nd commit

---
+++ server.php
@@ -0,0 +1,15 @@
+<?phpif(isset($_POST['reg_user']))
+{mysql_connect("localhost","root","Password")
+mysql_select_db("instruments");
+$user=$_POST['username'];
+$email=$_POST['email'];
+$psw1=$_POST['password_1'];
+$user=$_POST['password_2'];
+$qry="INSERT INTO reg(user,email,psw) VALUES('$user','$email','$psw1)";
+mysql_query($qry);
+mysql_close();
+mysql_query($qry);
+mysql_close();mysql_query($qry);
+mysql_close();
+}
+	
